08322d566ea37fa0f8b3b27c9648e59216be8bbf
erdemyildirim1 <93715110+erdemyildirim1@users.noreply.github.com>
2022-11-08 21:29:12 +0300

    Update spotify.py

--- spotify.py
+++ spotify.py
@@ -5,7 +5,7 @@ from matplotlib.pyplot import figure
 import numpy as np
 
 
-
+## Popularity of the Songs in Different Genres
 data=pd.read_csv("Datasets/SpotifyFeatures.csv")
 df=pd.DataFrame(data)
 
@@ -17,6 +17,23 @@ sns.color_palette("rocket",as_cmap=True)
 sns.barplot(y="popularity",x="genre",data=df)
 
 plt.xlabel("Genres")
+plt.xticks(rotation = 90)
+plt.ylabel("Popularity")
+plt.show()
+
+## Popularity of the Songs in Different Tempo
+
+data=pd.read_csv("Datasets/SpotifyFeatures.csv")
+df=pd.DataFrame(data)
+df["tempo_round"]=np.round(df["tempo"])
+
+figure(figsize=(150, 150), dpi=39)
+plt.subplots_adjust(bottom=0.25)
+plt.title("Popularity of the Songs in Different Tempo")
+sns.color_palette("rocket",as_cmap=True)
+sns.barplot(y="popularity",x="tempo_round",data=df)
+
+plt.xlabel("Tempo")
 plt.xticks(rotation = 90)
 plt.ylabel("Popularity")
 plt.show()
